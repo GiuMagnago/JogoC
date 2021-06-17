d0d7e4bfb01cda14915ed79425608b78db74134d
PedroGontijo13 <pedrotolentino2002@gmail.com>
2021-06-17 02:26:06 -0300

    Escolha De Classe Humana

--- exemplo.cpp
+++ exemplo.cpp
@@ -72,9 +72,59 @@ void Classes::Paladino() {
 };
 
 int main() {
-    Humanos *v1 = new Classes();
+    Classes *v1 = new Classes();
+    int opcao;
 
-    cout<<v1->pontosV;
+    do {
+        cout << "1 - Guerreiro" << "\n";
+        cout << "2 - Ladrao" << "\n";
+        cout << "3 - Mago" << "\n";
+        cout << "4 - Paladino()" << "\n";
+
+        cout << "Digite a opcao: ";
+        cin >> opcao;
+
+        if (opcao == 1) {
+            v1->Guerreiro();
+            cout << v1->pontosV << "\n";
+            cout << v1->pontosM << "\n";
+            cout << v1->forcaF << "\n";
+            cout << v1->forcaM << "\n";
+            cout << v1->armadura << "\n";
+            cout << v1->resistencia << "\n";
+            cout << v1->agilidade<<"\n";
+        }
+        if (opcao == 2) {
+            v1->Ladrao();
+            cout << v1->pontosV << "\n";
+            cout << v1->pontosM << "\n";
+            cout << v1->forcaF << "\n";
+            cout << v1->forcaM << "\n";
+            cout << v1->armadura << "\n";
+            cout << v1->resistencia << "\n";
+            cout << v1->agilidade<<"\n";
+        }
+        if (opcao == 3) {
+            v1->Mago();
+            cout << v1->pontosV << "\n";
+            cout << v1->pontosM << "\n";
+            cout << v1->forcaF << "\n";
+            cout << v1->forcaM << "\n";
+            cout << v1->armadura << "\n";
+            cout << v1->resistencia << "\n";
+            cout << v1->agilidade<<"\n";
+        }
+        if (opcao == 4) {
+            v1->Paladino();
+            cout << v1->pontosV << "\n";
+            cout << v1->pontosM << "\n";
+            cout << v1->forcaF << "\n";
+            cout << v1->forcaM << "\n";
+            cout << v1->armadura << "\n";
+            cout << v1->resistencia << "\n";
+            cout << v1->agilidade<<"\n";
+        }
+    } while(opcao != 0);
 
     delete v1;
 }
