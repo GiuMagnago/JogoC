64447f84716d4bcb145c0533f2a62d68266072b9
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2021-06-23 12:57:53 -0300

    Update Personagens.h

--- exemplo2-giu/Personagens.h
+++ exemplo2-giu/Personagens.h
@@ -18,7 +18,9 @@ class Personagens {
         int armas[7] = {1, 2, 3, 4, 5, 6, 7};
 
         //funções de ações
-        void recebeDano(int dano);
+        int escolheArma(int forcaF, int cod_arma);
+        int escolheMagia(int &pontosV, int &pontosM, int forcaM, int cod_magia);
+        int calculaEsquiva(int agil);
 
         //declaração das funções que altera os valores dos atributos de acordo com a escolha do personagem
         void Guerreiro();
@@ -30,15 +32,21 @@ class Personagens {
         void Dragao();
         void Zumbi();
 
+
+
+
         //funções das magias, organizadas na ordem do vetor de magias
 
-        int pocaoVida(); //posição "0" no vetor, valor "1"
-        int halitoFogo(); //posição "1" no vetor, valor "2"
-        int bio(); //posição "2" no vetor, valor "3"
-        int cura(); //posição "3" no vetor, valor "4"
-        int flamaGelada(); //posição "4" no vetor, valor "5"
-        int intoxicacao(); //posição "5" no vetor, valor "6"
-        int tempestade(); //posição "6" no vetor, valor "7"
+        void pocaoVida(int &pontosV, int &pontosM); //posição "0" no vetor, valor "1"
+        int halitoFogo(int &pontosM); //posição "1" no vetor, valor "2"
+        int bio(int &pontosM); //posição "2" no vetor, valor "3"
+        void cura(int &pontosV, int &pontosM); //posição "3" no vetor, valor "4"
+        int flamaGelada(int &pontosM); //posição "4" no vetor, valor "5"
+        int intoxicacao(int &pontosM); //posição "5" no vetor, valor "6"
+        int tempestade(int &pontosM); //posição "6" no vetor, valor "7"
+
+
+
 
         //funções das armas, organizadas na ordem do vetor de armas
 
@@ -50,11 +58,14 @@ class Personagens {
         int besta(); //posição "5" no vetor, valor "6"
         int esferaAtaque(); //posição "6" no vetor, valor "7"
 
+
         //funções para a espada lendária de Bul-Kathos
         int dadoBulKathos(); //caso o usuário deseje, rola o dado no inicio da rodada (20% de chance), caso ele consiga, ele usa a espada lendária por UMA RODADA
         int criticoKathos(); //caso o usuário consiga a espada lendária no dado, é sorteado se ele vai ter acerto crítico ou não (30% de chance)
-        
     public:
         //contrutor do personagem
         Personagens(int cod);
+
+        void recebeDano(int danoAtaque, int danoMagia, int &pontosV, int armor, int resist, int agil);
+        
 };
